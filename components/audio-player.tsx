bde36cd4634ef9035517a96bd86633e2ea6d2223
SamuelRnn <ricardo.reinn@outlook.com>
2023-06-16 02:18:30 -0500

    song model and mapping json

--- components/audio-player.tsx
+++ components/audio-player.tsx
@@ -3,8 +3,9 @@ interface Props {}
 export default function AudioPlayer({}: Props) {
   return (
     <section className='h-20 px-4 pb-2'>
-      <div className='py-2 flex w-full h-full'>
+      <div className='py-2 flex w-full h-full gap-8'>
         <div className='aspect-square rounded-md bg-zinc-800'></div>
+        <audio src='songs/2.wav' controls preload='true' />
       </div>
     </section>
   );
--- components/common/song-card/index.tsx
+++ components/common/song-card/index.tsx
@@ -1,28 +1,41 @@
 import Image from "next/image";
 import PlayButton from "./play-button";
+import { Song } from "@/models/song.model";
 
-interface Props {}
+interface Props {
+  songData: Song;
+}
 
-export default function SongCard({}: Props) {
+const DEFAULT: Song = {
+  id: "genérico-5n5vk34b",
+  title: "título de canción genérica",
+  artists: ["Gerardo Caminos", "artistas genéricos pero felices"],
+  coverRoute: "https://picsum.photos/200",
+};
+
+export default function SongCard({ songData = DEFAULT }: Props) {
   return (
     <div className='bg-black-charcoal p-4 transition-hover-intense aspect-[11/16] shadow-lg shadow-black-full/30 rounded-md hover:bg-gray-slate song-card'>
       <div className='relative'>
         <Image
-          src={"https://picsum.photos/200"}
+          src={songData.coverRoute}
           alt='image'
           width={200}
           height={200}
-          className='shadow-lg shadow-black-full/30 rounded-md w-full aspect-square bg-gray-slate'
+          className='shadow-lg object-cover shadow-black-full/30 rounded-md w-full aspect-square bg-gray-slate'
         />
+
         <div className='aspect-square absolute bottom-2 right-2 translate-y-3 opacity-0 cursor-pointer transition-hover-intense play-button '>
           <PlayButton className='hover:bg-green-lime-light' />
         </div>
       </div>
+
+      {/* song information */}
       <p className='overflow-hidden text-ellipsis whitespace-nowrap font-bold mt-4'>
-        título de canción genérico
+        {songData.title}
       </p>
       <p className='overflow-hidden text-ellipsis whitespace-nowrap mt-2 text-gray-mist'>
-        artista genérico
+        {songData.artists.join(", ")}
       </p>
     </div>
   );
